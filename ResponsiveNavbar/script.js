3a134e2c59535f9e41b672cadf0d15ad18e6086b
Raffly Maulana <117186735+HanyaRaff@users.noreply.github.com>
2023-10-17 19:14:12 +0700

    Add files via upload

---
+++ ResponsiveNavbar/script.js
@@ -0,0 +1,38 @@
+// const burger = document.getElementById('burger');
+// const items = document.getElementById('items-2');
+
+
+// burger.addEventListener('click', () => {
+//   // Toggle the 'show-flex' class on the element when the burger is clicked
+//   items.classList.toggle('show-flex');
+//   items.classList.toggle('hidden')
+// });
+
+// define all UI variable
+const navToggler = document.querySelector('.nav-toggler');
+const navMenu = document.querySelector('.site-navbar ul');
+const navLinks = document.querySelectorAll('.site-navbar a');
+
+// load all event listners
+allEventListners();
+
+// functions of all event listners
+function allEventListners() {
+  // toggler icon click event
+  navToggler.addEventListener('click', togglerClick);
+  // nav links click event
+  navLinks.forEach( elem => elem.addEventListener('click', navLinkClick));
+}
+
+// togglerClick function
+function togglerClick() {
+  navToggler.classList.toggle('toggler-open');
+  navMenu.classList.toggle('open');
+}
+
+// navLinkClick function
+function navLinkClick() {
+  if(navMenu.classList.contains('open')) {
+    navToggler.click();
+  }
+}
